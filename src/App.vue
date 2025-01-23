aae2e851a9e9eb8db702f11d860ba1d1f75748f5
zhizi1005 <zhzi1005@163.com>
2025-01-23 14:58:29 +0800

    walletConnect

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,79 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { createAppKit, useAppKit } from "@reown/appkit/vue";
+import { Ethers5Adapter } from "@reown/appkit-adapter-ethers5";
+import { mainnet, arbitrum, wemix } from "@reown/appkit/networks";
+import { useDisconnect } from "@reown/appkit/vue";
+import { ethers } from "ethers";
+
+const { disconnect } = useDisconnect();
+
+// 1. Get projectId at https://cloud.reown.com
+const projectId = "47f705dda8295fb99bd80b8a1fb93647";
+
+function getBlockchainApiRpcUrl(chainId) {
+  return `https://rpc.walletconnect.org/v1/?chainId=eip155:${chainId}&projectId=${projectId}`;
+}
+
+// 2. Create your application's metadata object
+const metadata = {
+  name: "My Website",
+  description: "My Website description",
+  url: "https://mywebsite.com", // url must match your domain & subdomain
+  icons: ["https://avatars.mywebsite.com/"],
+};
+
+// 3. Create a AppKit instance
+const modal = createAppKit({
+  adapters: [new Ethers5Adapter()],
+  networks: [mainnet, arbitrum, wemix],
+  projectId,
+  features: {
+    analytics: true, // Optional - defaults to your Cloud configuration
+  },
+});
+
+// 4. Use modal composable
+// const modal = useAppKit();
+
+const switchNetwork = (arg) => {
+  const networks = [mainnet, wemix];
+  modal.switchNetwork(networks[arg]);
+};
+
+const disConnect = async () => {
+  await disconnect();
+};
+const sendTransaction = async () => {
+  const walletProvider = modal.getWalletProvider();
+  const chainId = modal.getChainId();
+  console.log({ chainId });
+  console.log(walletProvider);
+
+  walletProvider.enable().then((res) => {
+    const provider = new ethers.providers.Web3Provider(walletProvider, chainId);
+    console.log(res[0]);
+    const signer = provider.getSigner(res[0]);
+    console.log(signer);
+    signer.sendTransaction({
+      to: "0xd0ff1B2BB47650083205e0b60A7c5548bf59A04A",
+      value: "1",
+    });
+  });
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div>
+    <appkit-button />
+    <button @click="switchNetwork(0)">切换到主网</button>
+    <button @click="switchNetwork(1)">切换到WEMIX</button>
+    <button @click="disConnect">断开链接</button>
+    <button @click="sendTransaction">发起交易</button>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+<style>
+button {
+  padding: 10px;
+  margin: 10px;
 }
 </style>
