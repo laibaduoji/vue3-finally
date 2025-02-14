46889c6b43c25e93d2c10d8e937507942cf87ed7
zhizi1005 <zhzi1005@163.com>
2025-02-14 17:14:41 +0800

    no message

--- src/views/wallet.vue
+++ src/views/wallet.vue
@@ -1,19 +1,54 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref } from "vue";
+import { createAppKit, useAppKit, useAppKitProvider } from "@reown/appkit/vue";
+import { EthersAdapter } from "@reown/appkit-adapter-ethers";
+import { mainnet, arbitrum } from "@reown/appkit/networks";
+import { BrowserProvider, Contract, formatUnits } from "ethers";
+
+// 1. Get projectId from https://cloud.reown.com
+const projectId = "a4def443738791ced991a65b128aca9a";
+
+// 2. Create your application's metadata object
+const metadata = {
+  name: "AEON_QR_PAY",
+  description: "AppKit Example",
+  url: "https://reown.com/appkit", // origin must match your domain & subdomain
+  icons: ["https://assets.reown.com/reown-profile-pic.png"],
+};
+
+// 3. Create a AppKit instance
+createAppKit({
+  adapters: [new EthersAdapter()],
+  networks: [mainnet, arbitrum],
+  metadata,
+  projectId,
+  features: {
+    analytics: true, // Optional - defaults to your Cloud configuration
+  },
+});
+
+// 4. Use modal composable
+const modal = useAppKit();
+console.log(modal);
+function getProvider() {
+  const { walletProvider } = useAppKitProvider("eip155");
+  const ethersProvider = new BrowserProvider(walletProvider);
+  console.log(ethersProvider);
+}
+</script>
 
 <template>
   <div>
-    <div>钱包app</div>
+    <appkit-button />
+    <appkit-connect-button />
+    <appkit-network-button />
+
+    <button @click="getProvider">getProvider</button>
   </div>
 </template>
 <style>
 button {
   padding: 10px;
   margin: 10px;
-}
-img,
-canvas {
-  width: 144px;
-  height: 144px;
-  border: 1px solid #ddd;
 }
 </style>
