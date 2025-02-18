6ff8105c7aa0bbcbee533a3e17d16d36e2faa8a8
zhizi1005 <zhzi1005@163.com>
2025-02-18 16:43:06 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -16,4 +16,22 @@ export default defineConfig({
     host: "0.0.0.0",
   },
   base: "./",
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          const { name, extname } = assetInfo;
+          return `assets/${name.replace(/_/g, "")}.${extname}`; // 替换掉文件名中的下划线
+        },
+        chunkFileNames: (chunkInfo) => {
+          const { name } = chunkInfo;
+          return `assets/${name.replace(/_/g, "")}.[hash].js`; // 替换掉文件名中的下划线
+        },
+        entryFileNames: (entryInfo) => {
+          const { name } = entryInfo;
+          return `assets/${name.replace(/_/g, "")}.[hash].js`; // 替换掉文件名中的下划线
+        },
+      },
+    },
+  },
 });
