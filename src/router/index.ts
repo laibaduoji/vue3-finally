b50ac2cb15f8eff1e83e85e23be19b78d35a562d
zhizi1005 <zhzi1005@163.com>
2025-02-20 18:21:33 +0800

    1

--- src/router/index.ts
+++ src/router/index.ts
@@ -32,20 +32,20 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import("../views/moralisweb3.vue"),
     },
-    /* {
+    {
       path: "/wallet",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import("../views/wallet.vue"),
-    },*/
-    /*{
+    },
+    {
       path: "/walletton",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import("../views/walletTon.vue"),
-    },*/
+    },
     {
       path: "/walletsol",
       // route level code-splitting
