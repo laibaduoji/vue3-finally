7abfa1456ba03b867d98ee10262ca79d0ed4ab3b
zhizi1005 <zhzi1005@163.com>
2025-03-06 18:13:40 +0800

    1

--- dist/assets/w3m-modal.pL-9rHe5.js
+++ dist/assets/w3m-modal.pL-9rHe5.js
@@ -1,4 +1,4 @@
-import{az as U,aA as I,aB as y,aC as i,aD as n,aE as k,aF as C,aG as u,aH as T,aI as h,aJ as N,U as L,i as O,aK as K,aL as R,aM as z,aN as P,aO as p,e as M,aP as $}from"./wallet.V7XFFL5D.js";import"./index.kFZbrBd3.js";import"./browser.E-OxKOAG.js";const W=U`
+import{az as U,aA as I,aB as y,aC as i,aD as n,aE as k,aF as C,aG as u,aH as T,aI as h,aJ as N,U as L,i as O,aK as K,aL as R,aM as z,aN as P,aO as p,e as M,aP as $}from"./wallet.CAcN-mgL.js";import"./index.CuaYOPWI.js";import"./browser.atVI88Vj.js";const W=U`
   :host {
     z-index: var(--w3m-z-index);
     display: block;
--- src/web3/evm/utils/wallet.ts
+++ src/web3/evm/utils/wallet.ts
@@ -35,7 +35,7 @@ const modal = createAppKit({
     emailShowWallets: false,
     swap: false,
   },
-  allWallets: "ONLY_MOBILE",
+  allWallets: "HIDE",
 
   includeWalletIds: [
     "38f5d18bd8522c244bdd70cb4a68e0e718865155811c043f052fb9f1c51de662", // bitget
