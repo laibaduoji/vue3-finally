a4ea1063cac54a27eb4ee420675654cef3ba3678
zhizi1005 <zhzi1005@163.com>
2025-03-06 18:14:06 +0800

    1

--- src/web3/evm/utils/wallet.ts
+++ src/web3/evm/utils/wallet.ts
@@ -38,7 +38,7 @@ const modal = createAppKit({
   allWallets: "HIDE",
 
   includeWalletIds: [
-    "38f5d18bd8522c244bdd70cb4a68e0e718865155811c043f052fb9f1c51de662", // bitget
+    // "38f5d18bd8522c244bdd70cb4a68e0e718865155811c043f052fb9f1c51de662", // bitget
     // "c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96", // metamask
   ],
   excludeWalletIds: [
